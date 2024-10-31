95f261f72ec2dc6330922e5a423200670575c534
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-10-31 20:10:46 +0800

    Add files via upload

---
+++ Prog-D.py
@@ -0,0 +1,5 @@
+# Buatlah program dengan output sebagai berikut
+print("Nama saya adalah Lalu Bagus Adi Kusuma Wijaya")
+print("NIM saya adalah 24241138")
+print("Saya berasal dari Lombok Timur, Desa Kota Raja")
+print("Nomor keberuntungan saya adalah 62")
