051999b68541c980719da3e8cd4a38756d4dcaaf
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-10-31 22:53:07 +0800

    Update Prog-D.py

--- Prog-D.py
+++ Prog-D.py
@@ -2,4 +2,5 @@
 print("Nama saya adalah Lalu Bagus Adi Kusuma Wijaya")
 print("NIM saya adalah 24241138")
 print("Saya berasal dari Lombok Timur, Desa Kota Raja")
-print("Nomor keberuntungan saya adalah 62")
+print("Nomor keberuntungan saya adalah (24+38)")
+print("62)
