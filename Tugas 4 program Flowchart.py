ef96ea6fae17871824366b9bff3d704b1c97dd5b
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-11-07 18:58:20 +0800

    Delete Tugas 4 program Flowchart.py

--- Tugas 4 program Flowchart.py
+++
@@ -1,9 +0,0 @@
-# Input lebar dan panjang 
-lebar = float(input("Masukkan lebar persegi panjang: "))
-panjang = float(input("Masukkan panjang persegi panjang: "))
-
-# Menghitung luas
-luas = panjang * lebar
-
-# Menampilkan hasil
-print("Luas persegi panjang adalah:", luas)
