9b598f1462efe555ce45d1d004cb5ecb106cb889
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-11-07 21:35:18 +0800

    Add files via upload

---
+++ TUGAS 4 PROGRAM FLOWCHART.py
@@ -0,0 +1,11 @@
+# Program Flowchart dengan variabel assigment value
+x = 30
+y = 20
+
+# Menghitung luas
+luas = x * y
+
+# Menampilkan hasil
+print("nilai x adalah :", x)
+print("nilai y adalah :", y)
+print("nilai luas adalah :", luas)
