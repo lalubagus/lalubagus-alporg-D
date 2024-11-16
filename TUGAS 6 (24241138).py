7afcca8acdf6df4e009ffa5256774b90f7bfb95c
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-11-17 02:41:06 +0800

    Update TUGAS 6 (24241138).py

--- TUGAS 6 (24241138).py
+++ TUGAS 6 (24241138).py
@@ -4,8 +4,10 @@
 lebar = 38
 panjang = 24
 
-# Proses perhitungan luas
+#hitung panjang lebar
 luas = panjang * lebar
 
 # Output hasil
+print("nilai lebar adalah:", lebar)
+print("nilai panjang adalah:", panjang)
 print(f"Luas persegi panjang adalah: {luas}")
