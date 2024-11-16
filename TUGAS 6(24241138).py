b10bc73b254b8bdd2e2d116c1c000a8a17fdf11e
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-11-16 15:27:04 +0800

    Add files via upload

---
+++ TUGAS 6(24241138).py
@@ -0,0 +1,14 @@
+# Maukan pengguna
+#casting tipe data
+b = input("Masukkan nilai b: ")
+print("Nilai b adalah:", b, "dengan tipe data", type(b))
+
+c = int(input("Masukkan nilai c: "))
+print("Nilai c adalah:", c, "dengan tipe data", type(c))
+
+d = float(input("Masukkan nilai d: "))
+print("Nilai d adalah:", d, "dengan tipe data", type(d))
+
+e = bool(int(input("Masukkan nilai e: ")))
+print("Nilai e adalah:", e, "dengan tipe data", type(e))
+    
