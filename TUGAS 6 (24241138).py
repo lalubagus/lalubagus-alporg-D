23cd21607d67dfd74c42fc8ae73906d2515482e6
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-11-17 02:27:23 +0800

    Add files via upload

---
+++ TUGAS 6 (24241138).py
@@ -0,0 +1,11 @@
+# Menghitung luas persegi panjang
+
+# Input panjang dan lebar
+lebar = float(input("Masukkan lebar: "))
+panjang = float(input("Masukkan panjang: "))
+
+# Proses perhitungan luas
+luas = panjang * lebar
+
+# Output hasil
+print(f"Luas persegi panjang adalah: {luas}")
