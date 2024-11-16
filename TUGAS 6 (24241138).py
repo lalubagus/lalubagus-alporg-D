330b435c51928aa3cb81c6873fd5f0c5d3a89c73
L∆l√_346US <103569321+lalubagus@users.noreply.github.com>
2024-11-16 15:28:12 +0800

    Add files via upload

---
+++ TUGAS 6 (24241138).py
@@ -0,0 +1,15 @@
+#modifikasi tugas 2
+
+x = 38
+y = 24
+luas = 912
+
+x = input("masukan nilai x:")
+print("nilai x adalah :",x)
+
+y = input("masukan nilai y:")
+print("nilai y adalah :",y)
+
+
+luas = input("masukan nilai luas:")
+print("nilai luas adalah :",luas)
